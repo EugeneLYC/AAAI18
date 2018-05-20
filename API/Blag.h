7b08a8a4c90c120eb91c4f6cb5a83a32f7d87900
EugeneLYC <randylyc@outlook.com>
2018-05-20 19:54:45 +0800

    reorganize repo

---
+++ API/Blag.h
@@ -0,0 +1,20 @@
+#include<iostream>
+using namespace std;
+
+struct ASG {
+    linkNode *asg;
+};
+
+
+
+class Blag {
+private:
+    struct ASG *actionSetGraph;
+    struct list list_of_neighbor;
+    char *msg_buffer;
+public:
+    Blag();
+    ~Blag();
+    void init_asg();
+    char *recv_command();
+};
